b588c25c2761381243790f08f16f54bffedaeda3
sergiubalogh <36952540+sergiubalogh@users.noreply.github.com>
2020-07-18 18:59:16 +0300

    node_modules be gone!

--- demoSite/src/app/app.module.ts
+++ demoSite/src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { AppRoutingModule,routingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { RouterModule } from '@angular/router';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    routingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule,
+    ReactiveFormsModule,
+    RouterModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- demoSite/src/environments/environment.ts
+++ demoSite/src/environments/environment.ts
@@ -3,7 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCo7n-m0faHjL9LntvrLxq8s_MP7D0JSMg",
+    authDomain: "demosite-b43b5.firebaseapp.com",
+    databaseURL: "https://demosite-b43b5.firebaseio.com",
+    projectId: "demosite-b43b5",
+    storageBucket: "demosite-b43b5.appspot.com",
+    messagingSenderId: "303120078823",
+    appId: "1:303120078823:web:a8e682e70a5f2842d74f42",
+    measurementId: "G-X0LNFDVYXL"
+  }
+  
 };
 
 /*
